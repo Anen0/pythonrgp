30dc7a2dd8c4edbf86fcdf28900930dc9aa1edb8
Nomad-linux <Anen0@github.com>
2025-12-19 00:55:34 +0800

    modified main.py

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 import tcod
 
 def main():
-    print('hello world')
+    screen_width = 80
+    screen_height = 50
+
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    player_x: int = screen_width // 2
+    player_y: int = screen_height // 2
+
+
+    with tcod.context.new(
+        tileset     =   tileset,
+        title       =   "Halllaaaaa",
+        vsync       =   True,
+    ) as context:
+        root_console = tcod.console.Console(screen_width, screen_height, order="F")
+        while True:
+            root_console.clear()
+            # root_console.print(x=5, y=5, string="@", fg=(255, 255, 0))
+            # root_console.print(x=1, y=1, string="@")
+            root_console.print(x=player_x, y=player_y, string="@", fg=(255, 255, 0))
+            print(root_console)
+
+            context.present(root_console)
+
+            for event in tcod.event.wait():
+                if event.type == "QUIT":
+                    raise SystemExit()
 
 if __name__ == "__main__":
     main()
